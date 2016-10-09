52d9dbc14d0f493c29327bbfc54ccc095dc39e6a
Chen <zcxp@live.cn>
2016-10-09 20:32:12 +0800

    Add show html

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as ts from 'typescript';
+import * as path from 'path';
 
 var output:vscode.OutputChannel;
 
@@ -19,7 +20,7 @@ export function activate(context: vscode.ExtensionContext) {
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('livecode.previewTypeScript', () => {      
         var document = vscode.window.activeTextEditor.document;
-        handleDocumentChange(document);
+        showPreview(document);
     });
     context.subscriptions.push(disposable);
 
@@ -52,6 +53,23 @@ function handleDocumentChange(document:vscode.TextDocument){
     output.appendLine(javascript);
     
 }
+
+function showPreview(document:vscode.TextDocument){
+    
+    var resource = document.uri;
+    var dynamicHtmlUrl= GetPreviewUri(resource); // "livecode:/preview?file:///c:/work/demo/test.ts"
+
+    // Error because livecode:// is unknown.
+    return vscode.commands.executeCommand('vscode.previewHtml',
+		dynamicHtmlUrl,
+		`Preview '${path.basename(resource.fsPath)}'`);
+}
+
+function GetPreviewUri(uri:vscode.Uri){
+    return uri.with({ scheme: 'livecode', path: 'preview', query: uri.toString() });
+}
+
+
 
 function isTypeScriptFile(document:vscode.TextDocument){
     return document.languageId === 'typescript'
